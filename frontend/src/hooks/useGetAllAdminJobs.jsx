900bd245606a090a4231284ad24a43e9efb3ed91
Siddharth kumar <siddharthraj1209@gmail.com>
2026-05-18 23:40:44 +0530

    updated backend and frontend changes

--- frontend/src/hooks/useGetAllAdminJobs.jsx
+++ frontend/src/hooks/useGetAllAdminJobs.jsx
@@ -11,7 +11,6 @@ const useGetAllAdminJobs = async() => {
         const fetchAllAdminJobs = async () => {
             try {
                 const res = await axios.get(`${JOB_API_END_POINT}/getadminjobs`,{withCredentials:true});
-        
                 if(res.data.success){
                     dispatch(setAllAdminJobs(res.data.jobs));
                 }
