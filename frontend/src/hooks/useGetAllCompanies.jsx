5dc641bb2e58491bc4826d6b4bbf08bd3f727439
Siddharth kumar <siddharthraj1209@gmail.com>
2026-05-20 00:47:33 +0530

    updated code

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -2,7 +2,7 @@ import { setCompanies} from '@/redux/companySlice'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-const COMPANY_API_END_POINT = import.meta.env.VVITE_COMPANY_API_END_POINT
+const COMPANY_API_END_POINT = import.meta.env.VITE_COMPANY_API_END_POINT
 
 const useGetAllCompanies =  () => {
     const dispatch = useDispatch();
